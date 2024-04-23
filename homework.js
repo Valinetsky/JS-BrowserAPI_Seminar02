9ddcdb66b4a93cafdb4fbe83a79d4fa7af4ee81a
Терёшин Роман <rtereshin@mac-55450192.users.rbc.ru>
2024-04-23 20:40:21 +0300

    Homework stable

--- homework.js
+++ homework.js
@@ -1,12 +1,21 @@
 "use strict";
+
+// Frame swap time
 const timeStep = 1000;
+// animationDelay
 const animationDelay = 500;
+// Slides quantity
 const maxElement = 9;
+// Center dot
 const middleDot = parseInt(maxElement / 2);
+// Images names array
 const images = [];
+// activeFrame
+let activeFrame = 0;
 
 const dotsFrame = document.querySelector(".slider__dots");
 
+// Creating dot element
 const dot = (position) => {
     const element = document.createElement("div");
     element.className = "dot";
@@ -16,12 +25,11 @@ const dot = (position) => {
     }
     return element;
 };
-
-let activeFrame = 0;
-
+// Modulo: maximum + 1 = minimum; minimum - 1 = maximum
 const frameModulo = (direction) =>
     (((activeFrame + direction) % maxElement) + maxElement) % maxElement;
 
+// Append images names to array, and dot elements to dotsFrame
 const arrayAndDotsGenerate = () => {
     for (let index = 0; index < maxElement; index++) {
         images.push(`${index + 1}.jpg`);
@@ -31,12 +39,19 @@ const arrayAndDotsGenerate = () => {
 
 arrayAndDotsGenerate();
 
+// Images in slider
 const frames = 3;
 
+// The prohibition flag
 let flag = true;
 
+// Slider
 const slider = document.querySelector(".slider");
 
+// Controls
+const controls = document.querySelector(".carousel");
+
+// Create and place frame to slider
 const getFrame = (direction) => {
     const currentFrame = frameModulo(direction);
     const sliderFrame = document.createElement("div");
@@ -49,41 +64,48 @@ const getFrame = (direction) => {
     return sliderFrame;
 };
 
+// initSlider
 const initSlider = () => {
     slider.append(getFrame(0));
     slider.append(getFrame(1));
     slider.prepend(getFrame(-1));
 };
 
+// animate frame on edge of slider
 function animate({ timing, draw, duration, removeElement }) {
     let start = performance.now();
 
     requestAnimationFrame(function animate(time) {
-        // timeFraction изменяется от 0 до 1
+        // timeFraction from 0 to 1
         let timeFraction = (time - start) / duration;
         if (timeFraction > 1) timeFraction = 1;
 
-        // вычисление текущего состояния анимации
+        // Current animation state
         let progress = timing(timeFraction);
 
-        draw(progress); // отрисовать её
+        draw(progress); // Drawing
 
         if (timeFraction < 1) {
             requestAnimationFrame(animate);
         } else {
+            // Remove frame
             removeElement.remove();
             flag = true;
         }
     });
 }
 
+// Adding new frame, animate, remove frame on different edge of slider
 const nextSlide = (direction) => {
+    // Prohibition of repeated animation
     if (!flag) {
         return;
     }
     flag = !flag;
+
     activeFrame = frameModulo(direction);
     const frameWidth = slider.offsetWidth / frames;
+
     let currentDiv;
 
     if (direction === 1) {
@@ -109,30 +131,40 @@ const nextSlide = (direction) => {
     });
 };
 
-const buttons = document.querySelector(".slider__buttons");
-buttons.addEventListener("click", function (event) {
-    // Start HERE
-    let eventClick = new Event("click");
-    elem.dispatchEvent(eventClick);
-    if (event.target.classList.contains("btn_left")) {
-        console.log("LEFT");
-        // nextSlide(-1);
-        dotClick(elem, -1);
-    }
-    if (event.target.classList.contains("btn_right")) {
-        console.log("right");
-        nextSlide(1);
-    }
-});
+// const buttons = document.querySelector(".slider__buttons");
+// buttons.addEventListener("click", function (event) {
+//     if (event.target.classList.contains("btn_left")) {
+//         console.log("LEFT");
+//         nextSlide(-1);
+//     }
+//     if (event.target.classList.contains("btn_right")) {
+//         console.log("right");
+//         nextSlide(1);
+//     }
+// });
 
+// dotsContainer
 const dotsContainer = document.querySelector(".slider__dots");
 
-function dotClick(event, direction = 0) {
+function controlClick(event) {
+    let dotsSteps = 1;
+    let direction;
+    let currentDot;
+    if (event.target.classList.contains("btn_left")) {
+        direction = -1;
+        currentDot = middleDot - 1;
+    }
+    if (event.target.classList.contains("btn_right")) {
+        direction = 1;
+        currentDot = middleDot + 1;
+    }
+    console.log(`First currentDot = ${currentDot}`);
     if (event.target.hasAttribute("data-pos")) {
-        dotsContainer.removeEventListener("click", dotClick);
-        const currentDot = parseInt(event.target.getAttribute("data-pos"));
-        let dotsSteps = currentDot - middleDot;
-        // let direction;
+        controls.removeEventListener("click", controlClick);
+        currentDot = parseInt(event.target.getAttribute("data-pos"));
+        dotsSteps = currentDot - middleDot;
+        console.log(`First dotsSteps = ${dotsSteps}`);
+
         if (dotsSteps !== 0) {
             if (dotsSteps < 0) {
                 direction = -1;
@@ -140,51 +172,58 @@ function dotClick(event, direction = 0) {
             } else {
                 direction = 1;
             }
-
-            // Animation parameters
-            const dotsTransform = [
-                { transform: "scale(1.5)", background: "white" },
-                { transform: "scale(1)" },
-            ];
-
-            const dotTiming = {
-                duration: timeStep * 0.5,
-                iterations: 1,
-            };
-            const dotHide = [
-                { transform: "scale(1)", background: "none" },
-                { transform: "scale(2)", background: "initial", offset: 1 },
-            ];
-            const dotHideTiming = {
-                duration: (timeStep + animationDelay) * (dotsSteps - 1),
-                iterations: 1,
-            };
-
-            // Middle dot animation
-            const middleDotAnimation = dotsContainer.childNodes[
-                middleDot
-            ].animate(dotHide, dotHideTiming, dotListener);
-            middleDotAnimation.onfinish = () => dotListener();
-
-            for (let i = 0; i < dotsSteps; i++) {
-                ((index) => {
-                    setTimeout(() => {
-                        dotsContainer.childNodes[
-                            currentDot + index * -direction - 1 * direction
-                        ].animate(dotsTransform, dotTiming);
-                        nextSlide(direction);
-                    }, (timeStep + animationDelay) * index);
-                })(i);
-            }
         }
+    }
+    console.log(`dotsSteps = ${dotsSteps}`);
+    console.log(`direction = ${direction}`);
+    frameMove(dotsSteps, direction, currentDot);
+}
+
+function frameMove(dotsSteps, direction, currentDot) {
+    // Animation parameters
+    const dotsTransform = [
+        { transform: "scale(1.5)", background: "white" },
+        { transform: "scale(1)" },
+    ];
+
+    const dotTiming = {
+        duration: timeStep * 0.5,
+        iterations: 1,
+    };
+    const dotHide = [
+        { transform: "scale(1)", background: "none" },
+        { transform: "scale(2)", background: "initial", offset: 1 },
+    ];
+    const dotHideTiming = {
+        duration: (timeStep + animationDelay) * (dotsSteps - 1),
+        iterations: 1,
+    };
+
+    // Middle dot animation
+    const middleDotAnimation = dotsContainer.childNodes[middleDot].animate(
+        dotHide,
+        dotHideTiming,
+        dotListener
+    );
+
+    middleDotAnimation.onfinish = () => dotListener();
+
+    // Dots animation from click to middleDot
+    for (let i = 0; i < dotsSteps; i++) {
+        ((index) => {
+            setTimeout(() => {
+                dotsContainer.childNodes[
+                    currentDot + index * -direction - 1 * direction
+                ].animate(dotsTransform, dotTiming);
+                nextSlide(direction);
+            }, (timeStep + animationDelay) * index);
+        })(i);
     }
 }
 
 function dotListener() {
-    // console.log("Add listener");
-    dotsContainer.addEventListener("click", dotClick);
+    controls.addEventListener("click", controlClick);
 }
 
-dotListener();
-
 initSlider();
+dotListener();
